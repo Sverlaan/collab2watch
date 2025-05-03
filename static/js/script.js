6c04d3ff75fe7d1dff9f84821fdbb8ad42a6e117
lianneroest <57528947+lianneroest@users.noreply.github.com>
2025-05-03 16:51:46 +0200

    Removed carousel next and prev buttons

--- static/js/script.js
+++ static/js/script.js
@@ -384,14 +384,8 @@ async function generateRewatchCarousels(allUsers, minRating, maxRating, minRunti
                 <div id="${carouselId}" class="carousel slide p-4" data-bs-ride="carousel">
                     <div class="carousel-inner" id="${carouselInnerId}"></div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#${carouselId}" data-bs-slide="prev">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" viewBox="0 0 16 16">
-                            <path d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-                        </svg>
                     </button>
                     <button class="carousel-control-next" type="button" data-bs-target="#${carouselId}" data-bs-slide="next">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" viewBox="0 0 16 16">
-                            <path d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-                        </svg>
                     </button>
                 </div>
             </div>
